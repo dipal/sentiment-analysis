f76611819f1dd318441cad6f7bdced1ed0249d75
Dipranjan Das <dipal.soul@gmail.com>
2017-10-07 02:34:54 +0600

    add plugin src

---
+++ sentiment-plugin/output/chrome/main.js
@@ -0,0 +1,16 @@
+﻿function Sentiment() {
+    var self = this;
+	alert("Loaded sentiment");
+    kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function() {
+        self._onCommand();
+    });
+}
+
+Sentiment.prototype = {
+
+    _onCommand: function() {
+        kango.browser.tabs.create({url: 'http://kangoextensions.com/'});
+    }
+};
+
+var extension = new Sentiment();
