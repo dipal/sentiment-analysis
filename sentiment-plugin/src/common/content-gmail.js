1ad0836c96cdf34f0d34bf2b237b2bc116b037d4
Dipranjan Das <dipal.soul@gmail.com>
2017-10-07 03:26:14 +0600

    add gmail.js

---
+++ sentiment-plugin/src/common/content-gmail.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name Sentiment
+// @include https://mail.google.com/
+// @include about:blank
+// @require jquery-3.2.1.min.js
+// ==/UserScript==
+
+var $ = window.$.noConflict(true); // Required for IE
+
+$(document).ready(function() {
+	kango.console.log('loaded gmail sentiment analysis tool');
+	$.post("https://45.79.170.176:8921/test", function(data, status){
+		kango.console.log('got data from ajax');
+    });
+});
